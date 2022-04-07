50dc43316a06bebb6b6d906c9568f8bb8041064c
LarsHoehn <90960806+fmAcvg@users.noreply.github.com>
2022-04-07 21:24:58 +0200

    initial

--- js/my.js
+++ js/my.js
@@ -2,7 +2,7 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const STAR_COUNT = 500
+const STAR_COUNT = 900
 let result = ""
 for(let i = 0; i < STAR_COUNT; i++){
     result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0, 3)}px #fff,`
