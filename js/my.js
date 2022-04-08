5194f830c9751f41024c1bda30490708f9f992be
LarsHoehn <90960806+fmAcvg@users.noreply.github.com>
2022-04-08 20:07:32 +0200

    respondable add

--- js/my.js
+++ js/my.js
@@ -7,12 +7,6 @@ let result = ""
 for(let i = 0; i < STAR_COUNT; i++){
     result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0, 3)}px #fff,`
 }
-console.log(result.substring(0, result.length - 1))
-
-var scroll = window.requestAnimationFrame ||
-    // IE Fallback
-    function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
 
 
